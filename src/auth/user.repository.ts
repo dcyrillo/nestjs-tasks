aca0426ef369ef5d986a0a6f315f37326673420d
dcyrillo <d.cyrillo@hotmail.com>
2019-10-07 20:58:58 -0300

    Hash password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { Repository, EntityRepository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import {
   ConflictException,
   InternalServerErrorException,
@@ -11,9 +12,12 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, salt);
     try {
       await user.save();
     } catch (error) {
@@ -23,5 +27,8 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
