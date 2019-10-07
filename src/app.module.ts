8f5f7292bc8dc8ecb43e091714487ffe2696cf2a
dcyrillo <d.cyrillo@hotmail.com>
2019-10-07 11:40:26 -0300

    Tinstance of modules of auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+
 
 
 @Module({
   imports: [TasksModule,
-            TypeOrmModule.forRoot(typeOrmConfig)],
+            TypeOrmModule.forRoot(typeOrmConfig),
+            AuthModule],
+  controllers: [AuthController],
 
 })
 export class AppModule {}
