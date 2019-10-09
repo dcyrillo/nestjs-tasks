1d2058fbe56540f22b5c539c8310482157b88e03
dcyrillo <d.cyrillo@hotmail.com>
2019-10-09 18:19:04 -0300

    token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,11 +16,13 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
-  async signIn(authCredentialsDto: AuthCredentialsDto) {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const username = await this.userRepository.validateUserPassword(
       authCredentialsDto,
     );
-    if (!username) {
+    if (username) {
       throw new UnauthorizedException('invalid credencials');
     }
     const payload: JwtPayload = { username };
